cf7b8ec282a87ec6ca0d2f4ebb259b589637344a
Orxan477 <tu201906147@code.edu.az>
2022-08-16 21:08:06 +0400

    Ders 3 changed name

---
+++ Ders 3 (If-else, For, While).py
@@ -0,0 +1,24 @@
+#region if else
+# x=-352843;
+
+# if(x>0):
+#     print("musbetdir");
+# elif(x<0):
+#     print("menfidir");
+# else:
+#     print("ne musbetdir nede menfi");
+#endregion
+
+#region For
+# a=[2,8,9,10];
+# for i in a:
+#     if(i%2==0):
+#         print(i);
+#endregion
+
+#region While
+# z=0
+# while(z<=3):
+#     print("Salam")
+#     z=z+1;
+#endregion
